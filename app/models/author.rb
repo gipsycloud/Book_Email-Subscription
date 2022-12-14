eb43ac297bc4cf375408bea89bb650ead72541a6
aungwin <aungwinygn1990@gmail.com>
2022-12-14 11:43:48 +0630

    adding validation in author page

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ApplicationRecord
- has_one_attached  :author_image       
+    has_one_attached  :author_image
+ 
+    validates :name, :email, :phone, :address, :author_image, presence: true
 end
